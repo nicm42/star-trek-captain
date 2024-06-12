44bae6b96780e9a7e3d5f43d7f260f7e2f4920c6
“nicm42” <42087659+nicm42@users.noreply.github.com>
2024-06-12 10:48:11 +0100

    Basically set up HTML structure

---
+++ src/components/Answer.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <input type="radio" name="answer" value="test-1" id="test-1" />
+  <label for="test-1">Test 1</label>
+  <input type="radio" name="answer" value="test-2" id="test-2" />
+  <label for="test-2">Test 2</label>
+  <input type="radio" name="answer" value="test-3" id="test-3" />
+  <label for="test-3">Test 3</label>
+</template>
+
+<style scoped></style>
---
+++ src/components/Question.vue
@@ -0,0 +1,32 @@
+<script setup>
+/* import { ref } from 'vue' */
+import Answer from './Answer.vue'
+
+defineProps({
+  msg: String
+})
+
+/* const count = ref(0) */
+</script>
+
+<template>
+  <!--<h1>{{ msg }}</h1>-->
+
+  <form>
+    <p>Question goes here</p>
+    <Answer />
+    <button type="submit">Next</button>
+  </form>
+
+  <!--
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+  -->
+</template>
+
+<style scoped></style>
